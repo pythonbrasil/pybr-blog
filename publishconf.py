42ad2f7c0b5dabde4a6ce57c943f0dec1675690d
Humberto Rocha <humrochagf@gmail.com>
2018-02-18 11:17:22 -0300

    fix https mixed contend

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://blog.pythonbrasil.org.br'
+SITEURL = 'https://blog.pythonbrasil.org.br'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
