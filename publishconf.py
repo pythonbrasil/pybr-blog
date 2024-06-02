3bb24c3f1c66c5b404a14c86e0634cc14ebb4ef7
Perceu <perceubertoletti@gmail.com>
2024-06-02 14:37:52 -0300

    fix url site

--- publishconf.py
+++ publishconf.py
@@ -8,7 +8,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = "http://pythonbrasil.github.io/pybr-blog/"
+SITEURL = "https://blog.pythonbrasil.org.br/"
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = "feeds/all.atom.xml"
